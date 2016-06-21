6f326cb1f9e6bbbd1fc5baba62d9378eb5705a51
Nick Ford <nickford@Nicks-Mac.local>
2016-06-21 10:49:29 +0100

    address set to 0x3F

---
+++ Pcb_ArdBir_DanielXan.h
@@ -0,0 +1,16 @@
+// sensor and lcd
+OneWire ds(7);
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+// push buttons
+const char Button_dn    = A3;
+const char Button_up    = A2;
+const char Button_enter = A1;
+const char Button_start = A0;
+
+// outputs
+const byte Pump = 6;
+const byte Buzz = 8;
+const byte Heat = 9;
+
+
