d25473c7b8f5b822465aef68a138bc56d23130a0
Nick Ford <nickford@Nicks-Mac.local>
2016-06-21 10:53:52 +0100

    folder

---
+++ OpenAdBir_I2C/Pcb_14.h
@@ -0,0 +1,16 @@
+// sensor and lcd
+OneWire ds(8);
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+// push buttons
+const char Button_up    = A3;
+const char Button_dn    = A2;
+const char Button_start = A1;
+const char Button_enter = A0;
+
+// outputs
+const byte Heat = 11;
+const byte Pump = 9;
+const byte Buzz = 10;
+
+
---
+++ OpenAdBir_I2C/README.md
@@ -0,0 +1,2 @@
+# Open-ArdBir-for-I2C-LCD
+A fork of Open ArdBir that integrates support for the use of I2C LCD
